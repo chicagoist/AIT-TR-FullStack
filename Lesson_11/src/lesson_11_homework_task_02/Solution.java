abf93f84a8b0532ba030810cc6094a521d58da36
chicagoist <le.legioner@gmail.com>
2023-11-22 00:59:03 +0100

    fix comment in code

--- Lesson_11/src/lesson_11_homework_task_02/Solution.java
+++ Lesson_11/src/lesson_11_homework_task_02/Solution.java
@@ -2,6 +2,17 @@ package lesson_11_homework_task_02;
 
 import java.util.Scanner;
 
+/*
+Написать метод, выводящий на печать треугольник, состоящий из цифр от 1 до
+числа n такого вида:
+
+1
+12
+123
+1234
+12345
+123456
+ */
 
 public class Solution {
     public static void main(String[] args) {
