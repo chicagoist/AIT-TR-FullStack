71c8905f28201e4de3fc8cc4d530ddef6ea7fdb7
chicagoist <le.legioner@gmail.com>
2023-11-22 09:10:38 +0100

    Task 02 without String

--- Lesson_11/src/lesson_11_homework_task_02/Solution.java
+++ Lesson_11/src/lesson_11_homework_task_02/Solution.java
@@ -26,7 +26,7 @@ public class Solution {
 
     }
 
-    private static void printTriangle(int number) {
+/*    private static void printTriangle(int number) {
         int index = 1;
         String line = "";
 
@@ -35,5 +35,26 @@ public class Solution {
             System.out.println(line);
             index++;
         }
+    }*/
+
+        private static void printTriangle(int number) {
+
+        int firstIndex = 1;
+
+        while(firstIndex <= number) {
+
+            int secondIndex = 1;
+
+            while(secondIndex <= firstIndex) {
+                System.out.print(secondIndex);
+                secondIndex++;
+            }
+            System.out.println("");
+            firstIndex++;
+        }
     }
+
+
+
+
 }
