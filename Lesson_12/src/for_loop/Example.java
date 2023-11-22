702dbb5eb8d6a2ab1d85b73e9b3a4a4aaad3a0c2
chicagoist <le.legioner@gmail.com>
2023-11-22 11:35:32 +0100

    sum for()

--- Lesson_12/src/for_loop/Example.java
+++ Lesson_12/src/for_loop/Example.java
@@ -2,9 +2,9 @@ package for_loop;
 
 public class Example {
     public static void main(String[] args) {
-        int i;
+
         int sum = 0;
-        for (i = 0; i <= 5; i++) {
+        for (int i = 1; i <= 5; i++) {
             sum = sum + i;
         }
         System.out.println(sum);
