50c995cf0eea58098ce95d3ac3a4bd6b1b017714
chicagoist <le.legioner@gmail.com>
2023-11-24 10:03:21 +0100

    Как удалить любую папку, в т.ч. и .idea

--- Lesson_13/src/lesson_13_git/Git.java
+++ Lesson_13/src/lesson_13_git/Git.java
@@ -1,12 +1,30 @@
 package lesson_13_git;
 
-public class Git {
-    public static void main(String[] args) {
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
-        System.out.println("Hello, Git!"); // 5556666
+public class Git {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hello, Git!");
+        FileReader fr = new FileReader("Lesson_13/src/lesson_13_git/HowToIgnoreIdea.txt");
+        Scanner scan = new Scanner(fr);
+
+        int i = 1;
+
+        while(scan.hasNextLine()) {
+            System.out.println(i + " : " + scan.nextLine());
+            i++;
+        }
+
+        fr.close();
+
 
     }
+
 }
+
 /*
 Как удалить любую папку, в т.ч. и .idea, и лишние файлы из Git
 
@@ -16,7 +34,8 @@ echo ".idea" > .gitignore
 
 
 ------------------------------------------------------------------
-Подробнее можно ознакомиться на странице: https://git-scm.com/book/ru/v1/Основы-Git-Запись-изменений-в-репозиторий
+Подробнее можно ознакомиться на странице: https://git-scm
+.com/book/ru/v1/Основы-Git-Запись-изменений-в-репозиторий
 Удаляем ненужные папки и файлы при синхронизации в Git репозиторий
 
 Удалим все проиндексированные файлы:
