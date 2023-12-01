8bbc494ffe55a62133807309d35aa6beb2046186
chicagoist <le.legioner@gmail.com>
2023-12-01 12:59:18 +0100

    добавил новый метод checkAnagram

--- Lesson_17/src/lesson_17_classwork/Solution.java
+++ Lesson_17/src/lesson_17_classwork/Solution.java
@@ -9,10 +9,41 @@ public class Solution {
 
         char[] str = info.toCharArray();
         int len = str.length;
-        removeDuplicateElements(str, len);
+        System.out.println("removeDuplicateElements : " + removeDuplicateElements(str, len));
 
         // добавил новый метод
-        System.out.println("removeDuplicateV2 : "+removeDuplicateV2(str));
+        System.out.println("removeDuplicateV2 : " + removeDuplicateV2(str));
+
+        //Анаграмма
+        String anagramStr1 = "abac";
+        String anagramStr2 = "caba";
+        String anagramStr4 = "cccc";
+        String anagramStr3 = "abc";
+        System.out.println("Is it a Anagram? : " + checkAnagram(anagramStr1,anagramStr2));
+        System.out.println("Is it a Anagram? : " + checkAnagram(anagramStr1,anagramStr4));
+        System.out.println("Is it a Anagram? : " + checkAnagram(anagramStr1,anagramStr3));
+    }
+
+    private static boolean checkAnagram(String anagramStr1, String anagramStr2) {
+
+        boolean result = false;
+        int index = 0;
+
+        if(anagramStr1.length() != anagramStr2.length()) {
+            return false;
+        }
+
+        char[] content1 = anagramStr1.toCharArray();
+        Arrays.sort(content1);
+        anagramStr1 = Arrays.toString(content1);
+
+        char[] content2 = anagramStr2.toCharArray();
+        Arrays.sort(content2);
+        anagramStr2 = Arrays.toString(content2);
+
+
+
+        return anagramStr1.equals(anagramStr2);
     }
 
     private static String removeDuplicateV2(char[] str) {
@@ -23,13 +54,12 @@ public class Solution {
             if(result.indexOf(c) == -1) {
                 result = result + c;
             }
-
         }
 
         return result;
     }
 
-    static void removeDuplicateElements(char[] str, int length) {
+    static String removeDuplicateElements(char[] str, int length) {
         int index = 0;
 
         for(int i = 0; i < length; i++) {
@@ -45,7 +75,7 @@ public class Solution {
                 str[index++] = str[i];
             }
         }
-        System.out.println(String.valueOf(Arrays.copyOf(str, index)));
+        return (String.valueOf(Arrays.copyOf(str, index)));
     }
 }
 
