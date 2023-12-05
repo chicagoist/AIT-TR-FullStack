c92068828a3849f5e52c5e9b3a0a876bf4028385
chicagoist <Araf"T1974>
2023-12-05 13:00:03 +0100

    Method main in different Class Main

---
+++ Lesson_14/src/lesson_14_homework_task_01/Main.java
@@ -0,0 +1,28 @@
+package lesson_14_homework_task_01;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] numbersArray0 = {1, 2, 3, 4, 5};
+        int[] numbersArray1 = {1, 2, 3, 4, 6};
+        int[] numbersArray2 = {1, 2, 3, 4, 5};
+
+        System.out.println("###########################################");
+        System.out.println("numbersArray0 have numbers: " + Arrays.toString(numbersArray0));
+        System.out.println("numbersArray1 have numbers: " + Arrays.toString(numbersArray1));
+        System.out.println("numbersArray2 have numbers: " + Arrays.toString(numbersArray2));
+        System.out.println("###########################################");
+
+
+        System.out.println("Compare arrays numbersArray0 and numbersArray1 is" +
+                " = " + Solution.compare(numbersArray0, numbersArray1));
+        System.out.println("Compare arrays numbersArray0 and numbersArray2 is" +
+                " = " + Solution.compare(numbersArray0, numbersArray2));
+        System.out.println("Compare arrays numbersArray1 and numbersArray2 is" +
+                " = " + Solution.compare(numbersArray1, numbersArray2));
+
+    }
+}
---
+++ Lesson_14/src/lesson_14_homework_task_02/Main.java
@@ -0,0 +1,23 @@
+package lesson_14_homework_task_02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbersArray0 = {1, 2, 3, 4, 5};
+        int[] numbersArray1 = {1, 2, 3, 5, 7};
+        int[] numbersArray2 = {2};
+        int[] numbersArray3 = {};
+
+
+        System.out.println("The number of even numbers in numbersArray0 is = "
+                + Solution.evenNumbers(numbersArray0));
+        System.out.println("The number of even numbers in numbersArray1 is = "
+                + Solution.evenNumbers(numbersArray1));
+        System.out.println("The number of even numbers in numbersArray2 is = "
+                + Solution.evenNumbers(numbersArray2));
+        System.out.println("The number of even numbers in numbersArray3 is = "
+                + Solution.evenNumbers(numbersArray3));
+
+    }
+
+}
