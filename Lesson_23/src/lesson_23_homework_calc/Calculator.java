a5f107bb22818a910abb6515bf39431dfd7adb42
chicagoist <le.legioner@gmail.com>
2023-12-11 21:02:03 +0100

    Lesson 23 Homework Calculator

---
+++ Lesson_23/src/lesson_23_homework_calc/Calculator.java
@@ -0,0 +1,218 @@
+package lesson_23_homework_calc;
+
+public class Calculator {
+    /*
+    Методы принимают аргументы и int и double, но результат выводится
+    только в double
+    Не уверен, что правильно понял ТЗ, но ниже закомментированы методы на
+    все остальные случаи.
+    */
+    public void addingTwoNumbers(double operand1, double operand2) {
+        System.out.println("Результат сложения первого операнда " + operand1
+                + " и второго операнда " + operand2 + " будет равен = " +
+                (operand1 + operand2));
+    }
+
+
+    public void subtractingTwoNumbers(double operand1, double operand2) {
+        System.out.println("Результат вычитания из первого операнда " + operand1
+                + ", второго операнда " + operand2 + " будет равен = " +
+                (operand1 - operand2));
+
+    }
+
+    public void addingThreeNumbers(double operand1, double operand2,
+                                   double operand3) {
+        System.out.println("Результат сложения первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда " + operand3 +
+                " будет равен = " +
+                (operand1 + operand2 + operand3));
+    }
+
+    public void subtractingThreeNumbers(double operand1, double operand2,
+                                        double operand3) {
+        System.out.println("Результат вычитания первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда " + operand3 +
+                " будет равен = " +
+                (operand1 - operand2 - operand3));
+    }
+
+
+
+
+
+/*
+
+    public void addingTwoNumbers(int operand1, int operand2) {
+        System.out.println("Результат сложения первого операнда " + operand1
+                + " и второго операнда " + operand2 + " будет равен = " +
+                (operand1 + operand2));
+    }
+
+    public void addingTwoNumbers(int operand1, double operand2) {
+        System.out.println("Результат сложения первого операнда " + operand1
+                + " и второго операнда " + operand2 + " будет равен = " +
+                (operand1 + operand2));
+    }
+
+    public void addingTwoNumbers(double operand1, int operand2) {
+        System.out.println("Результат сложения первого операнда " + operand1
+                + " и второго операнда " + operand2 + " будет равен = " +
+                (operand1 + operand2));
+    }
+
+
+
+    public void subtractingTwoNumbers(double operand1, int operand2) {
+        System.out.println("Результат вычитания из первого операнда " + operand1
+                + ", второго операнда " + operand2 + " будет равен = " +
+                (operand1 - operand2));
+
+    }
+
+    public void subtractingTwoNumbers(int operand1, double operand2) {
+        System.out.println("Результат вычитания из первого операнда " + operand1
+                + ", второго операнда " + operand2 + " будет равен = " +
+                (operand1 - operand2));
+
+    }
+
+    public void subtractingTwoNumbers(int operand1, int operand2) {
+        System.out.println("Результат вычитания из первого операнда " + operand1
+                + ", второго операнда " + operand2 + " будет равен = " +
+                (operand1 - operand2));
+
+    }
+
+    public void addingThreeNumbers(int operand1, int operand2, int operand3) {
+        System.out.println("Результат сложения первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 + operand2 + operand3));
+    }
+
+    public void addingThreeNumbers(double operand1, int operand2,
+                                   int operand3) {
+        System.out.println("Результат сложения первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 + operand2 + operand3));
+    }
+
+
+    public void addingThreeNumbers(double operand1, double operand2,
+                                   int operand3) {
+        System.out.println("Результат сложения первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 + operand2 + operand3));
+    }
+
+
+
+    public void addingThreeNumbers(int operand1, double operand2,
+                                   double operand3) {
+        System.out.println("Результат сложения первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 + operand2 + operand3));
+    }
+
+    public void addingThreeNumbers(int operand1, int operand2,
+                                   double operand3) {
+        System.out.println("Результат сложения первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 + operand2 + operand3));
+    }
+
+    public void addingThreeNumbers(double operand1, int operand2,
+                                   double operand3) {
+        System.out.println("Результат сложения первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 + operand2 + operand3));
+    }
+
+    public void addingThreeNumbers(int operand1, double operand2,
+                                   int operand3) {
+        System.out.println("Результат сложения первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 + operand2 + operand3));
+    }
+
+
+    public void subtractingThreeNumbers(int operand1, int operand2,
+                                        int operand3) {
+        System.out.println("Результат вычитания первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 - operand2 - operand3));
+    }
+
+    public void subtractingThreeNumbers(double operand1, int operand2,
+                                        int operand3) {
+        System.out.println("Результат вычитания первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 - operand2 - operand3));
+    }
+
+    public void subtractingThreeNumbers(double operand1, double operand2,
+                                        int operand3) {
+        System.out.println("Результат вычитания первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 - operand2 - operand3));
+    }
+
+
+    public void subtractingThreeNumbers(int operand1, double operand2,
+                                        double operand3) {
+        System.out.println("Результат вычитания первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 - operand2 - operand3));
+    }
+
+    public void subtractingThreeNumbers(int operand1, int operand2,
+                                        double operand3) {
+        System.out.println("Результат вычитания первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 - operand2 - operand3));
+    }
+
+    public void subtractingThreeNumbers(double operand1, int operand2,
+                                        double operand3) {
+        System.out.println("Результат вычитания первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 - operand2 - operand3));
+    }
+
+    public void subtractingThreeNumbers(int operand1, double operand2,
+                                        int operand3) {
+        System.out.println("Результат вычитания первого операнда " + operand1
+                + ", второго операнда " + operand2 + " и третьего операнда "
+                + operand3 +
+                " будет равен = " +
+                (operand1 - operand2 - operand3));
+    }
+*/
+
+}
---
+++ Lesson_23/src/lesson_23_homework_calc/Main.java
@@ -0,0 +1,53 @@
+package lesson_23_homework_calc;
+
+import java.util.Scanner;
+
+/*
+Задача 1
+
+Создать класс Calculator, способный выполнять два арифметических действия -
+сложение двух чисел, вычитание двух чисел а также сложение трех чисел и
+вычитание трех чисел.
+Калькулятор должен работать как для целых чисел, так и для чисел с запятой.
+(т.е. в классе надо создать соответствующие методы и затем вызывать их в
+методе main)
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        calculator.addingTwoNumbers(-2.0,3);
+        calculator.addingTwoNumbers(2.0,3);
+        calculator.addingTwoNumbers(2,3.0);
+        calculator.addingTwoNumbers(2,3);
+        calculator.addingTwoNumbers(2.0,3.0);
+        System.out.println("");
+        calculator.subtractingTwoNumbers(-2.0,3);
+        calculator.subtractingTwoNumbers(2.0,3);
+        calculator.subtractingTwoNumbers(2,3.0);
+        calculator.subtractingTwoNumbers(2,3);
+        calculator.subtractingTwoNumbers(2.0,3.0);
+        System.out.println("");
+        calculator.addingThreeNumbers(2,3,2);
+        calculator.addingThreeNumbers(2.0,3,2);
+        calculator.addingThreeNumbers(2.0,3.0,2);
+        calculator.addingThreeNumbers(2.0,3.0,2.0);
+        calculator.addingThreeNumbers(2,3.0,2.0);
+        calculator.addingThreeNumbers(2,3,2.0);
+        calculator.addingThreeNumbers(2.0,3,2.0);
+        calculator.addingThreeNumbers(2,3.0,2);
+        System.out.println("");
+        calculator.subtractingThreeNumbers(4,5,6);
+        calculator.subtractingThreeNumbers(4.0,5,6);
+        calculator.subtractingThreeNumbers(4.0,5.0,6);
+        calculator.subtractingThreeNumbers(4.0,5.0,6.0);
+        calculator.subtractingThreeNumbers(4,5.0,6.0);
+        calculator.subtractingThreeNumbers(4,5,6.0);
+        calculator.subtractingThreeNumbers(4.0,5,6.0);
+        calculator.subtractingThreeNumbers(4,5.0,6);
+
+
+    }
+}
