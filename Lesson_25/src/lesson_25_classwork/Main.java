a4cb8ee4238b9ddd0f0d3039b01b49d7e684fa5f
chicagoist <le.legioner@gmail.com>
2023-12-14 15:23:16 +0100

    Lesson 25, Classwork testing

---
+++ Lesson_25/src/lesson_25_classwork/Main.java
@@ -0,0 +1,9 @@
+package lesson_25_classwork;
+
+public class Main {
+    public static void main(String[] args) {
+        TestArrays testArrays = new TestArrays();
+        testArrays.add(5);
+        System.out.println(testArrays.toString());
+    }
+}
---
+++ Lesson_25/src/lesson_25_classwork/TestArrays.java
@@ -0,0 +1,18 @@
+package lesson_25_classwork;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class TestArrays extends ArrayList {
+
+  //protected   ArrayList testarray = new ArrayList();
+
+    public void add(int valu) {
+
+        System.out.println("value = " + valu);
+       this.add(valu);
+
+
+    }
+}
