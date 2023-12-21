45c5d25afcc1d5df909851ad1d698cf588b56e02
chicagoist <le.legioner@gmail.com>
2023-12-21 09:38:24 +0100

    fix with Scanner

--- Lesson_30/src/lesson_30_homework_enum/Main.java
+++ Lesson_30/src/lesson_30_homework_enum/Main.java
@@ -1,8 +1,15 @@
 package lesson_30_homework_enum;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Day askDay = new Day("sunday");
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter a your day: ");
+        String enterdDay = myScanner.nextLine();
+
+        Day askDay = new Day(enterdDay);
         System.out.println(askDay.askForWeekend());
     }
 }
