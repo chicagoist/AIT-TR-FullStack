ef3a407a5179175efd6e570494fda34d363f0857
chicagoist <le.legioner@gmail.com>
2024-01-09 19:39:01 +0100

    Wrappers / Обёртки

---
+++ Lesson_34/src/lesson_34_classwork_generics/Main.java
@@ -0,0 +1,14 @@
+package lesson_34_classwork_generics;
+
+public class Main { //Wrappers
+    public static void main(String[] args) {
+
+        Integer objInteger = new Integer(1);
+        Integer objInteger1 = Integer.valueOf(-1);
+        Integer objInteger2 = 2;
+
+        System.out.println(objInteger);
+        System.out.println(objInteger1);
+        System.out.println(objInteger2);
+    }
+}
