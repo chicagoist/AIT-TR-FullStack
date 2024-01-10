d82553e752200b0429797de481d9c289923a7b8d
chicagoist <le.legioner@gmail.com>
2024-01-10 23:32:27 +0100

    Lesson 35 Homework Sort for id and firstName

--- Lesson_35/src/lesson_35/homework/sort_of_accounting/Main.java
+++ Lesson_35/src/lesson_35/homework/sort_of_accounting/Main.java
@@ -53,28 +53,18 @@ public class Main {
 
 /* OUTPUT
 Total list of all employees after sorting by name:
-WageEmployee{wage=25.0}Employee{id=2, firstName='Jane', lastName='Smith',
-hour=160.0}
-WageEmployee{wage=140.0}Employee{id=3, firstName='Jane', lastName='Dou',
-hour=160.0}
-WageEmployee{wage=40.0}Employee{id=4, firstName='Jane', lastName='Mac',
-hour=60.0}
-SalesManager{salesVolume=20000.0, percent=0.1}Employee{id=1,
-firstName='John', lastName='Young', hour=160.0}
-SalesManager{salesVolume=25000.0, percent=0.3}Employee{id=4,
-firstName='Jurgen', lastName='Flint', hour=100.0}
+WageEmployee{wage=25.0}Employee{id=2, firstName='Jane', lastName='Smith',hour=160.0}
+WageEmployee{wage=140.0}Employee{id=3, firstName='Jane', lastName='Dou',hour=160.0}
+WageEmployee{wage=40.0}Employee{id=4, firstName='Jane', lastName='Mac',hour=60.0}
+SalesManager{salesVolume=20000.0, percent=0.1}Employee{id=1,firstName='John', lastName='Young', hour=160.0}
+SalesManager{salesVolume=25000.0, percent=0.3}Employee{id=4,firstName='Jurgen', lastName='Flint', hour=100.0}
 
 Total list of all employees after sorting by id:
-SalesManager{salesVolume=20000.0, percent=0.1}Employee{id=1,
-firstName='John', lastName='Young', hour=160.0}
-WageEmployee{wage=25.0}Employee{id=2, firstName='Jane', lastName='Smith',
-hour=160.0}
-WageEmployee{wage=140.0}Employee{id=3, firstName='Jane', lastName='Dou',
-hour=160.0}
-WageEmployee{wage=40.0}Employee{id=4, firstName='Jane', lastName='Mac',
-hour=60.0}
-SalesManager{salesVolume=25000.0, percent=0.3}Employee{id=4,
-firstName='Jurgen', lastName='Flint', hour=100.0}
+SalesManager{salesVolume=20000.0, percent=0.1}Employee{id=1,firstName='John', lastName='Young', hour=160.0}
+WageEmployee{wage=25.0}Employee{id=2, firstName='Jane', lastName='Smith',hour=160.0}
+WageEmployee{wage=140.0}Employee{id=3, firstName='Jane', lastName='Dou',hour=160.0}
+WageEmployee{wage=40.0}Employee{id=4, firstName='Jane', lastName='Mac',hour=60.0}
+SalesManager{salesVolume=25000.0, percent=0.3}Employee{id=4,firstName='Jurgen', lastName='Flint', hour=100.0}
 
 Process finished with exit code 0
 
