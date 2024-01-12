8d78d9106db6c1ac2264b68a68d11edbbf567d21
chicagoist <chicagoist@exploit.im>
2024-01-12 09:47:20 +0100

    More test

--- Lesson_36/src/lesson_36/homework/CircleTest.java
+++ Lesson_36/src/lesson_36/homework/CircleTest.java
@@ -8,10 +8,22 @@ class CircleTest {
     @DisplayName("Тест площади для радиуса 10.5")
     public void testCalculateArea() {
         Shape circle = new Circle(10.5);
+        Shape circle1 = new Circle(0.5);
+        Shape circle2 = new Circle(105);
+        Shape circle3 = new Circle(1000);
         //S = π · r2
         double expectedArea = Math.PI * Math.pow(10.5, 2);
+        double expectedArea1 = Math.PI * Math.pow(0.5, 2);
+        double expectedArea2 = Math.PI * Math.pow(105, 2);
+        double expectedArea3 = Math.PI * Math.pow(1000, 2);
         double actualArea = circle.calculateArea();
+        double actualArea1 = circle1.calculateArea();
+        double actualArea2 = circle2.calculateArea();
+        double actualArea3 = circle3.calculateArea();
         Assertions.assertEquals(expectedArea, actualArea);
+        Assertions.assertEquals(expectedArea1, actualArea1);
+        Assertions.assertEquals(expectedArea2, actualArea2);
+        Assertions.assertEquals(expectedArea3, actualArea3);
     }
 
     @Test
