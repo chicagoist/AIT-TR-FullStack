9c093ace30691bc8f631009ee8ed10f4c82b29ae
chicagoist <chicagoist@exploit.im>
2024-01-19 09:53:37 +0100

    fix to use HashMap

--- Lesson_41/src/lesson_41/homework/MapProcessor.java
+++ Lesson_41/src/lesson_41/homework/MapProcessor.java
@@ -1,6 +1,8 @@
 package lesson_41.homework;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
 // ПСЕВДОКОД:
@@ -17,13 +19,29 @@ import java.util.List;
 public class MapProcessor<T> {
 
   public int countOccurrences(List<T> list, T _name)  {
-    int index = 0;
+    Map<T, Integer> occurrencesMap = new HashMap<>();
+
+    // Заполняем HashMap с количеством вхождений каждого элемента
+    for (T item : list) {
+      if (occurrencesMap.containsKey(item)) {
+        occurrencesMap.put(item, occurrencesMap.get(item) + 1);
+      } else {
+        occurrencesMap.put(item, 1);
+      }
+    }
+
+    // Возвращаем количество вхождений для заданного элемента
+    return occurrencesMap.containsKey(_name) ? occurrencesMap.get(_name) : 0;
+  }
+
+
+/*    int index = 0;
     for (T item : list) {
       if (item.equals(_name)) {
         index++;
       }
     }
     return index;
-  }
+  }*/
 
 }
