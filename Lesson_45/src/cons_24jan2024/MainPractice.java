ab5e9dbdc5a5b7a35f25147532c7561e62bf9fcb
chicagoist <chicagoist@exploit.im>
2024-01-24 19:15:42 +0100

    Cons 24.01.2024

--- Lesson_45/src/cons_24jan2024/MainPractice.java
+++ Lesson_45/src/cons_24jan2024/MainPractice.java
@@ -1,21 +1,17 @@
 package cons_24jan2024;
 
-/*import java.util.function.Function;
+import java.util.function.Function;
 import java.util.function.Predicate;
-*/
+
 
 public class MainPractice {
 
     public static void main(String[] args) {
         String str = "aaa BBBB dDD eeEEe lll";
-        //   Predicate<String> predicate = (s) -> s.length() == 3;
-        //  Function<String, String> function = String::toUpperCase;
+        Predicate<String> predicate = (s) -> s.length() == 3;
+        Function<String, String> function = String::toUpperCase;
 
-/*        String result1 = Practice.transformer("aaa BBBB dDD eeEEe lll",
-                s -> s.length() == 3,
-                s -> s.toUpperCase());
-        */
-        //  String result1 = Practice.transformer(str, predicate, function);
+        String result1 = Practice.transformer(str, predicate, function);
 
         String result2 = Practice.transformer(str,
                 s -> s.length() == 4,
@@ -26,7 +22,7 @@ public class MainPractice {
                 s -> "*****");
 
         // Вывод результатов
-        //    System.out.println(result1);
+        System.out.println(result1);
         System.out.println(result2);
         System.out.println(result3);
 
