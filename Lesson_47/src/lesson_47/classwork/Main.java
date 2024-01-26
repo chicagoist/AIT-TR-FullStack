e6a6e8777b69c480c7d72446ed4b798ceb424c3c
chicagoist <chicagoist@exploit.im>
2024-01-26 13:01:08 +0100

    Lesson 47 Classwork

--- Lesson_47/src/lesson_47/classwork/Main.java
+++ Lesson_47/src/lesson_47/classwork/Main.java
@@ -1,8 +1,23 @@
 package lesson_47.classwork;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        String[] arrayString = {"Hello","Java"};
+        Arrays.stream(arrayString)
+                .map(str -> str.split(""))
+                .map(Arrays::stream)
+                .forEach(System.out::println);
+
+        // FlatMap
+        String[] arrayStringFlat = {"Hello","Java"};
+        Arrays.stream(arrayString)
+                .map(str -> str.split(""))
+                .flatMap(Arrays::stream)
+                .forEach(System.out::print);
 
     }
 
