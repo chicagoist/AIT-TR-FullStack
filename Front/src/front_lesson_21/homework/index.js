2193bf513abdc396010e79e5871e5f6ef6e409dc
chicagoist <le.legioner@gmail.com>
2024-02-29 18:40:28 +0100

    Lesson 21 Homework

--- Front/src/front_lesson_21/homework/index.js
+++ Front/src/front_lesson_21/homework/index.js
@@ -1,49 +1,7 @@
-// Character
-let character = {
-  name: "New Character",
-  health: 100,
-  level: 1,
+class Transport {
 
-  introduce() {
-    console.log(`"Hello, my name is ${this.name}, I am at level ${this.level}, and I have ${this.health} health."`);
-  },
-};
+  constructor(props) {
 
-//Warrior
-let warrior = {
-  name: "New Warrior",
-  health: 200,
-  level: 2,
-  weapon: ["Knife", "Sword"],
-
-  attack() {
-    const index = Math.floor(Math.random() * this.weapon.length); // choosing a weapon to attack
-    console.log(`Атакую с помощью ${this.weapon[index]} !!!`);
-  },
-  __proto__: character,
-};
-
-// Wizard
-let wizard = {
-  name: "New Wizard",
-  health: 1000,
-  level: 10,
-  spell: "NWO",
-
-  castSpell() {
-    console.log(`The wizard ${this.name} casts spells: ${this.spell}!!!`);
-  },
-  __proto__: character,
-};
-
-character.introduce();
-warrior.introduce();
-warrior.attack();
-wizard.introduce();
-wizard.castSpell();
-
-class Wizard {
-
-  constructor() {
   }
+
 }
