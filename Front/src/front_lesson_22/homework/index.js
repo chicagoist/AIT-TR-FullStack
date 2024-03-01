d9c2293d9f9094306fd19e9826a83d3c6f730763
chicagoist <le.legioner@gmail.com>
2024-03-01 20:11:18 +0100

    Lesson 22 Homework done

--- Front/src/front_lesson_22/homework/index.js
+++ Front/src/front_lesson_22/homework/index.js
@@ -1,56 +1,26 @@
-class Transport {
-  constructor(_make, _model, _year) {
-    this.make = _make;
-    this.model = _model;
-    this.year = _year;
+const homeworkDiv = document.querySelector(".homework22>div");
+const button = document.querySelector(".countdown");
+let count = 60; // начально значение на странице
+homeworkDiv.textContent = count.toString(); // отображение начального значение счётчика
+let isButtonInUse = false;
+
+const timer = () => {
+  let countTimeFunc = () => {
+    count--;
+    if (count >= 0) {
+      homeworkDiv.textContent = count.toString().padStart(2, "0"); // двухциферное отображение
+    } else if (count <= 0) {
+      clearInterval(intervalSec); // STOP
+    }
+  };
+  let intervalSec = setInterval(countTimeFunc, 1000);
+};
+
+// BUTTON
+button.addEventListener("click", () => {
+  if (!isButtonInUse) {
+    // чтобы кнопка не срабатывала во время счёта
+    isButtonInUse = !isButtonInUse;
+    timer();
   }
-
-  displayMainInfo() {
-    console.log(`This model \"${this.model}\" produced by ${this.make} in ${this.year}`);
-  }
-}
-
-// Car
-class Car extends Transport {
-  constructor(_make, _model, _year, _numDoors, _isConvertible) {
-    super(_make, _model, _year);
-    this.numDoors = _numDoors;
-    this.isConvertible = _isConvertible;
-  }
-
-  displayMainInfo() {
-    console.log(
-      `This model \"${this.model}\" produced by ${this.make} in ${this.year} and the car have a ${this.numDoors} doors. Is it convertible? ${this.isConvertible}!`,
-    );
-  }
-}
-
-// Motorcycle
-class Motorcycle extends Transport {
-  constructor(_make, _model, _year, _numWheels, _hasSideCar) {
-    super(_make, _model, _year);
-    this.numWheels = _numWheels;
-    this.hasSideCar = _hasSideCar;
-  }
-
-  displayMainInfo() {
-    console.log(
-      `This model \"${this.model}\" produced by ${this.make} in ${this.year} and the moto have a ${this.numWheels} wheels. Has sidecar? ${this.hasSideCar}!`,
-    );
-  }
-}
-
-// let vehicle = new Transport('Ford', 'Taurus',  1998);
-// vehicle.displayMainInfo();
-
-let car1 = new Car("Mercedes", "Vito", 2010, 4, false);
-car1.displayMainInfo();
-
-let car2 = new Car("Mazda", "Miata", 1998, 2, Boolean(true));
-car2.displayMainInfo();
-
-let moto1 = new Motorcycle("Jawa", 350, 1996, 2, Boolean(false));
-moto1.displayMainInfo();
-
-let moto2 = new Motorcycle("Ural", "Волк", 2020, 3, Boolean(true));
-moto2.displayMainInfo();
+});
