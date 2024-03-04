554f1c66c2f5d0ebc99fc0c08c4c6e5df49093c0
chicagoist <le.legioner@gmail.com>
2024-03-04 13:21:15 +0100

    Lesson 22 Homework disabled BUTTON

--- Front/src/front_lesson_22/homework/index.js
+++ Front/src/front_lesson_22/homework/index.js
@@ -1,26 +1,25 @@
 const homeworkDiv = document.querySelector(".homework22>div");
 const button = document.querySelector(".countdown");
-let count = 60; // начально значение на странице
-homeworkDiv.textContent = count.toString(); // отображение начального значение счётчика
-let isButtonInUse = false;
+let count = 10; // начальное значение на странице
+homeworkDiv.textContent = count.toString().padStart(2, "0"); // отображение начального значения счетчика
+let intervalSec;
 
-const timer = () => {
-  let countTimeFunc = () => {
+const startTimer = () => {
+  count = 10;
+  homeworkDiv.textContent = count.toString().padStart(2, "0");
+  button.setAttribute('disabled', true); // Делаем кнопку неактивной во время счетчика
+  intervalSec = setInterval(() => {
     count--;
     if (count >= 0) {
-      homeworkDiv.textContent = count.toString().padStart(2, "0"); // двухциферное отображение
-    } else if (count <= 0) {
-      clearInterval(intervalSec); // STOP
+      homeworkDiv.textContent = count.toString().padStart(2, "0"); // двузначное отображение
+    } else {
+      clearInterval(intervalSec); // Остановка таймера
+      button.removeAttribute('disabled'); // После остановки таймера кнопка снова доступна
     }
-  };
-  let intervalSec = setInterval(countTimeFunc, 1000);
+  }, 1000);
 };
 
-// BUTTON
+// Обработчик кнопки
 button.addEventListener("click", () => {
-  if (!isButtonInUse) {
-    // чтобы кнопка не срабатывала во время счёта
-    isButtonInUse = !isButtonInUse;
-    timer();
-  }
+    startTimer(); // Запуск таймера
 });
