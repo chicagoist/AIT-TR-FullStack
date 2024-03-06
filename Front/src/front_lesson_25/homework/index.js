6c7682245302316e3dbe87c5bdb1afaf404130d0
chicagoist <chicagoist@exploit.im>
2024-03-06 21:17:40 +0100

    Lesson 25 homework styleblock

--- Front/src/front_lesson_25/homework/index.js
+++ Front/src/front_lesson_25/homework/index.js
@@ -7,7 +7,17 @@ const getDataButton = document.querySelector(".get-data");
 // <div>
 const div25 = document.createElement("div");
 
-const getRandomCat = async () => {
+// Стилизация блока ответов и ошибки
+const styleBlock = () => {
+  homeworkContainer.style.margin = "10px";
+  homeworkContainer.style.borderColor = "#ccc";
+  homeworkContainer.style.borderWidth = "2px";
+  homeworkContainer.style.borderRadius = "5px";
+  homeworkContainer.style.backgroundColor = "#9fd1eb";
+  homeworkContainer.style.padding = "20px";
+};
+
+const getRandomCat = async (callback) => {
   try {
     const responseFromServer = await fetch("https://catfact.ninja/fact", {
       method: "GET",
@@ -16,6 +26,9 @@ const getRandomCat = async () => {
       },
     });
 
+    // //Error test
+    // const responseFromServer = Object.assign(new Error("Some response error message"));
+
     if (!responseFromServer.ok) {
       throw Object.assign(new Error("Some response error message"), {
         response: responseFromServer,
@@ -23,47 +36,19 @@ const getRandomCat = async () => {
     } else {
       const respondFact = await responseFromServer.json();
 
-      homeworkContainer.style.margin = "10px";
-      homeworkContainer.style.borderColor = "#ccc";
-      homeworkContainer.style.borderWidth = "2px";
-      homeworkContainer.style.borderRadius = "5px";
-      homeworkContainer.style.backgroundColor = "#9fd1eb";
-      homeworkContainer.style.padding = "20px";
-
+      // Заполнение div
       div25.textContent = `${respondFact["fact"]}`;
       homeworkContainer.append(div25);
+      callback();
     }
   } catch (error) {
-    console.error(`Ошибка ${error.message}`);
+    homeworkContainer.style.color = "#fc0505";
+    div25.textContent = `Ошибка ${error.message}`;
+    styleBlock();
+    homeworkContainer.append(div25);
   }
 };
 
-getDataButton.addEventListener("click", getRandomCat);
-
-/*
-
-Методы HTTP возвращают различные типы ответов, которые содержат информацию о выполнении запроса и результате операции. Вот типичные возвращаемые значения для каждого метода:
-
-1. **GET**: Возвращает данные запрошенного ресурса. Обычно это текст, JSON, XML или другой формат данных, который был запрошен.
-
-2. **POST**: Обычно возвращает информацию о созданном ресурсе. Это может быть ID созданного объекта, ссылка на него или какие-то другие метаданные.
-
-3. **PUT**: Возвращает обычно информацию о ресурсе, который был обновлен. Это может быть обновленное представление ресурса или какие-то другие метаданные.
-
-4. **PATCH**: Обычно возвращает информацию о ресурсе, который был частично обновлен. Также может возвращать обновленное представление ресурса или какие-то другие метаданные.
-
-5. **DELETE**: Обычно возвращает подтверждение удаления ресурса или информацию о удаленном ресурсе.
-
-6. **HEAD**: Возвращает только метаданные о запрашиваемом ресурсе без его фактического содержания. Это может включать в себя информацию о заголовках ответа, коде состояния и другие метаданные.
-
-7. **OPTIONS**: Возвращает информацию о возможностях сервера или ресурса. Это может быть список поддерживаемых методов, доступных заголовков и другая информация о конфигурации сервера.
-
-8. **TRACE**: Возвращает запрос, который был отправлен на сервер, что может быть полезно для диагностики и отладки.
-
-9. **CONNECT**: Возвращает установленное соединение.
-
-10. **PATCH**: Обычно возвращает информацию о ресурсе, который был частично обновлен.
-
-    Это общие типы ответов для каждого метода HTTP, но конкретные ответы могут варьироваться в зависимости от реализации сервера и контекста запроса.
-
-*/
+getDataButton.addEventListener("click", async () => {
+  await getRandomCat(styleBlock);
+});
